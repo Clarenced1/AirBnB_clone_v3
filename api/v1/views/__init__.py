d62725c567d864871de5cb0ba5950e0db956f787
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-30 19:17:02 +0300

    API status

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,5 @@
+from api.v1.views.index import api_status  # Import first
+
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# Avoid wildcard import and import the specific views
-from api.v1.views.index import api_status
