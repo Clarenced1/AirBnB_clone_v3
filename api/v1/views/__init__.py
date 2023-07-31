c51c762fa922f68a2b5c6698a581492ba4c3c194
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-31 10:58:21 +0300

    fix - __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,9 @@
 """This module define a blueprint for routes with Blueprint object"""
 from flask import Blueprint
 
+app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
+
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -9,8 +12,4 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
-<<<<<<< HEAD
 from api.v1.views.places_amenities import *
-=======
-from api.v1.views.places_amenities import *
->>>>>>> 83918d98bab2266b6c32569ab05930cc72d15348
