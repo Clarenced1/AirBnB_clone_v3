f55c78ebd9342081825dd5235e17655d64dc49d4
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-31 11:20:24 +0300

    search - api/v1/views/places.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,8 +2,6 @@
 """create blueprint"""
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
 if app_views is not None:
     from api.v1.views.index import *
     from api.v1.views.states import *
@@ -13,3 +11,5 @@ if app_views is not None:
     from api.v1.views.places import *
     from api.v1.views.places_reviews import *
     from api.v1.views.places_amenities import *
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
